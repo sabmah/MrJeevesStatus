ff22823b43daebed89d880b995438c618028cfd6
Sabin Maharjan <sabin.mhrj@gmail.com>
2015-11-30 14:04:35 -0800

    Update models.py
    removed commas from the models

--- MrJeevesApp/models.py
+++ MrJeevesApp/models.py
@@ -7,19 +7,19 @@ class ROSActionType(models.Model):
         return self.action_type
     
 class ROSActionRegister(models.Model):
-    action_type = models.ForeignKey(ROSActionType),
-    name = models.CharField(max_length=128, unique=True),
-    server_name = models.CharField(max_length=128, unique=True),
-    client_name = models.CharField(max_length=128, unique=True),
-    publisher_name = models.CharField(max_length=128, unique=True),
-    subsriber_name = models.CharField(max_length=128, unique=True),
+    action_type = models.ForeignKey(ROSActionType)
+    name = models.CharField(max_length=128, unique=True)
+    server_name = models.CharField(max_length=128, unique=True)
+    client_name = models.CharField(max_length=128, unique=True)
+    publisher_name = models.CharField(max_length=128, unique=True)
+    subsriber_name = models.CharField(max_length=128, unique=True)
     
     def __unicode__(self):
         return self.name
     
 class ROSActionOutput(models.Model):
-    ros_action = models.ForeignKey(ROSActionRegister),
-    data = models.TextField(), #Stores Raw JSON Data
+    ros_action = models.ForeignKey(ROSActionRegister)
+    data = models.TextField() #Stores Raw JSON Data
     
     def __unicode__(self):
         return self.data
